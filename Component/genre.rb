661018414503eadc4d8ed04d917ad743cd80d979
girma <girmatarekegn.gi@gmail.com>
2023-02-28 12:35:07 +0300

    Add genre class initializetion of constractor

--- Component/genre.rb
+++ Component/genre.rb
@@ -1,3 +1,10 @@
 class Genre
-  puts 'Class Genre'
+  attr_accessor :name
+  attr_reader :id, :items
+
+  def initialze(name)
+    @id = Randome.rand(1...1000)
+    @name = name
+    @items = []
+  end
 end
