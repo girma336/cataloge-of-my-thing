21646421306b7ac1e32a00988741f9e07718766d
AhmadiGu <ahmadigulalai76@gmail.com>
2023-03-01 17:42:11 +0500

    Add code to json file data

--- classes/book.rb
+++ classes/book.rb
@@ -10,6 +10,17 @@ class Book < Item
     super(publish_date)
   end
 
+  def to_json(*_args)
+    {
+      'publish_date' => @publish_date,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state,
+      'archived' => @archived
+    }
+  end
+
+  private
+
   def can_be_archived?
     super || @cover_state == 'bad'
   end
