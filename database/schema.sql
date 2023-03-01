3df5783af3b71bfef576d0966ea1d5877cdecd7a
AhmadiGu <ahmadigulalai76@gmail.com>
2023-03-01 20:47:59 +0500

    Fixed the errors

--- database/schema.sql
+++ database/schema.sql
@@ -20,19 +20,19 @@ CREATE TABLE books (
 
 DROP TABLE IF EXISTS genre
 CREATE TABLE genre (
-    id SERIAL NOT NULL PRIMARY key,
+    id SERIAL  PRIMARY key,
     name VARCHAR(100) NOT NULL
 )
 
 DROP TABLE IF EXISTS itemType
 CREATE TABLE itemType (
-    id SERIAL NOT NULL PRIMARY key,
+    id SERIAL  PRIMARY key,
     name VARCHAR(100)
 )
 
 DROP TABLE IF EXISTS item
 CREATE TABLE item(
-    id SERIAL NOT NULL PRIMARY key,
+    id SERIAL  PRIMARY key,
     publish_date DATA NOT NULL,
     archived BOOLEAN,
     genre_id INT,
