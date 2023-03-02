6dc6c063bda945658415a82e6423469da2601d97
BryteLitty <littyofficials@gmail.com>
2023-03-02 10:24:18 +0000

    fix linter errors

--- classes/author.rb
+++ classes/author.rb
@@ -1,17 +1,16 @@
 class Author
-    attr_accessor :first_name, :last_name, :items
-    attr_reader :id
+  attr_accessor :first_name, :last_name, :items
+  attr_reader :id
 
-    def initialize(_id, first_name, last_name)
-        @id = Random.rand(1...1000)
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+  def initialize(_id, first_name, last_name)
+    @id = Random.rand(1...1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.author = self
-    end
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
 end
-
--- classes/game.rb
+++ classes/game.rb
@@ -1,14 +1,14 @@
 class Game
-    attr_accessor :multiplayer, :last_played_at
-    attr_reader :id
+  attr_accessor :multiplayer, :last_played_at
+  attr_reader :id
 
-    def initialize(id, publish_date, archived, multiplayer, last_played_at)
-        super(id, publish_date, archived: archived)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(id, publish_date, archived, multiplayer, last_played_at)
+    super(id, publish_date, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def can_be_archived?
-        return (super && (Time.now - @last_played_at) > (2 * 365 * 24 * 60 * 60))
-    end
+  def can_be_archived?
+    (super && (Time.now - @last_played_at) > (2 * 365 * 24 * 60 * 60))
+  end
 end
